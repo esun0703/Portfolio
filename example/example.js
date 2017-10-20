e24b2b0d52f60eaa427db33661b79fb947d1fd65
Emily Sun <emilysun@Emilys-MacBook-Pro.local>
2017-10-20 18:18:49 -0400

    removed unncessary files

--- example/example.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.navbuttoncontainer').addEventListener('click', function (e) {
-  var target = e.target;
-  
-  if (target.tagName === 'A') {
-    e.currentTarget.querySelector('.current').classList.remove('current');
-    target.classList.add('current');
-  }
-});
